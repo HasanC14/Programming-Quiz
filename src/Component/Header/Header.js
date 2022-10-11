5e24f4d40e74ef56181ffc383739ea37349643f1
HasanC14 <chowdhuryh999@gmail.com>
2022-10-12 05:07:03 +0600

    Header Added

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header p-10">
+      <p className="text-center md:text-6xl text-4xl">
+        Learn Programming with Fun Quiz
+      </p>
+      <p className="text-center md:text-4xl text-xl mt-6">
+        Practice on 1000+ Questions
+      </p>
+    </div>
+  );
+};
+
+export default Header;
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import Header from "../Header/Header";
 import Topic from "../Topic/Topic";
 
 const Home = () => {
   const LoadTopics = useLoaderData();
   const Topics = LoadTopics.data;
   return (
-    <div className="grid lg:grid-cols-2 sm:grid-cols-1 m-10 gap-4">
-      {Topics.map((topic) => (
-        <Topic key={topic.id} topic={topic}></Topic>
-      ))}
+    <div>
+      <section className="mt-8">
+        <Header></Header>
+      </section>
+      <div className="grid md:grid-cols-2 grid-cols-1 m-10 gap-4">
+        {Topics.map((topic) => (
+          <Topic key={topic.id} topic={topic}></Topic>
+        ))}
+      </div>
     </div>
   );
 };
