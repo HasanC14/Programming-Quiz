84f204651086f0ae47ec5d6653b84a3dd9717e2c
HasanC14 <chowdhuryh999@gmail.com>
2022-10-12 19:01:00 +0600

    Header text responsive

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <p className="text-center md:text-6xl text-4xl font-bold text-white">
           Learn Programming with Fun Quiz
         </p>
-        <p className="text-center md:text-4xl text-xl mt-6 text-white md:ml-40 md:mr-40 m-0">
+        <p className="text-center md:text-4xl text-xl mt-6 text-white md:ml-10 md:mr-10 lg:ml-40 lg:mr-40 m-0">
           Get ready to practice on <span className="font-bold">1000+</span>{" "}
           questions. These quizzes will help you focus, identify gaps in
           knowledge, and build confidence.So, what are you waiting for?
